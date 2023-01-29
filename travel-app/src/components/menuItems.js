f8204d248716573a464bb56213711c8247a7e7b8
mohammad usman mir <mirusman21421@gmail.com>
2023-01-29 20:27:17 +0530

    navbar

---
+++ travel-app/src/components/menuItems.js
@@ -0,0 +1,34 @@
+export const menuItems=[
+{
+    title: "Home",
+    url:"/",
+    cName:"nav-Links",
+    icon:"fa-solid fa-house-chimney"
+},
+{
+    title: "About",
+    url:"/about",
+    cName:"nav-Links",
+    icon:"fa-regular fa-address-card"
+},
+{
+    title: "Service",
+    url:"/service",
+    cName:"nav-Links",
+    icon:"fa-solid fa-suitcase"
+},
+{
+    title: "Contact",
+    url:"/contact",
+    cName:"nav-Links",
+    icon:"fa-solid fa-address-book"
+},
+{
+    title: "Sign up",
+    url:"/signup",
+    cName:"nav-links-mobile"
+    
+},
+
+
+]
